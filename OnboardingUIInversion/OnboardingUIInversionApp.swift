7145336fe6d33e567f38ea7c3db526bc1efd168e
kingpogi123 <43062730+kingpogi123@users.noreply.github.com>
2021-11-03 22:34:22 +0800

    Initial Commit

---
+++ OnboardingUIInversion/OnboardingUIInversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingUIInversionApp.swift
+//  OnboardingUIInversion
+//
+//  Created by King Copones on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingUIInversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
